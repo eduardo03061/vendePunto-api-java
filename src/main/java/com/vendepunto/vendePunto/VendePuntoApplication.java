3473d6e0441c7500604638e86f682036d763896b
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2025-06-03 17:55:57 -0600

    users endpoint

--- src/main/java/com/vendepunto/vendePunto/VendePuntoApplication.java
+++ src/main/java/com/vendepunto/vendePunto/VendePuntoApplication.java
@@ -2,8 +2,11 @@ package com.vendepunto.vendePunto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.vendepunto.controller", "com.vendepunto.vendePunto"})
 public class VendePuntoApplication {
 
 	public static void main(String[] args) {
