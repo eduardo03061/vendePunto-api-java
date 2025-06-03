f56196b3a77409ff87a5a6488454263e178b4ef0
Eduardo Hernandez <eduardoalejandrohg@gmail.com>
2025-06-03 17:26:11 -0600

    codebase

---
+++ src/main/java/com/vendepunto/vendePunto/VendePuntoApplication.java
@@ -0,0 +1,13 @@
+package com.vendepunto.vendePunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendePuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendePuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendepunto/vendePunto/VendePuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendepunto.vendePunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendePuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
